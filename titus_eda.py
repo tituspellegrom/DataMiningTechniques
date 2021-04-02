46fbe189bdb371f7cccb77c9bea346ea1fa1830d
Titus Pellegrom <tituspellegrom@gmail.com>
2021-04-02 22:41:14 +0200

    Feature Aggregate & Heatmap
    1) DataFrame in wide-format
    2) Data Aggregation per date
    3) Average daily mood heatmap
    4) Papers

--- titus_eda.py
+++ titus_eda.py
@@ -1,13 +1,25 @@
+import numpy as np
 import pandas as pd
 import plotly.graph_objects as go
 import plotly.io as pio
+from collections import ChainMap
 pio.renderers.default = "browser"
 
 DATA_FOLDER = 'data/'
 FILE_NAME = 'dataset_mood_smartphone.csv'
 
 df = pd.read_csv(DATA_FOLDER+FILE_NAME, index_col=0, parse_dates=['time'])
-print(df.dtypes)
+
+# TODO: remove negative time values
+df.dropna(subset=['value'], inplace=True)
+df['date'] = df['time'].dt.date
+
+# TODO: Decide on duplicates removal => or let pivot_table handle?
+# df = df.set_index(['id', 'time', 'variable']).sort_index()
+# duplicates = df[df.index.duplicated(keep=False)]
+# df = df.groupby(level=[df.index.names]).agg({'value': np.mean})    # Remove duplicates by taking mean
+# duplicates2 = df[df.index.duplicated(keep=False)]
+
 
 # Check which users we have, and how much
 users = df['id'].unique().tolist()
@@ -17,17 +29,32 @@ n_users = len(users)
 expected_users = {f"AS14.{i+1:02d}" for i in range(n_users)}
 missing_users = expected_users.difference(users)
 
-# how often per day are measurements made?
-df['date'] = df['time'].dt.date
-df_timeline = df.groupby(['id', 'variable', 'date']).size().unstack(level=0)
+# Full feature aggregation per day
+group_score = ['mood', 'circumplex.arousal', 'circumplex.valence', 'activity']
+group_count = ['call', 'sms']
+group_time = [col for col in df.columns if col.startswith('appCat.') or col == 'screen']
 
-# Plot nr. of mood logs per user
-df_moods = df_timeline.query('variable == "mood"').reset_index(level=0, drop=True)
-fig = go.Figure([go.Scatter(x=df_moods.index, y=df_moods[usr], mode='lines+markers', name=usr)
-                 for usr in df_moods.columns])
-fig.show() # between 0 and 6 mood logs per day => some gaps
+agg_fn_score = {col: ['min', 'max', 'median', 'mean', 'std', 'count'] for col in group_score}
+agg_fn_count = {col: ['count'] for col in group_score}
+agg_fn_time = {col: ['min', 'max', 'median', 'mean', 'std', 'count', 'sum'] for col in group_time}
+agg_dict = ChainMap(agg_fn_score, agg_fn_count, agg_fn_time)
 
+df_wide = df.copy()
+df_wide = df_wide.pivot_table(index=['id', 'date'], columns='variable', values='value',
+                              aggfunc=['min', 'max', 'median', 'mean', 'std', 'count', 'sum'])
+df_wide = df_wide.swaplevel(0, 1, axis=1).sort_index(level='variable', axis=1)
+dups = df_wide[df_wide.index.duplicated(keep=False)]    # suddenly duplicates??
+df_wide.drop_duplicates(keep='last', inplace=True)
 
+# heatmap mood
+df_mood = df_wide.loc[:, ('mood', 'mean')].unstack(level='date').sort_index(level='date', axis=1)
+fig = go.Figure(data=go.Heatmap(z=df_mood.values,
+                                x=df_mood.columns,
+                                y=df_mood.index.values,
+                                colorscale='rdylgn',
+                                zmid=5.5
+                                ))
+fig.show()
 
 
 
