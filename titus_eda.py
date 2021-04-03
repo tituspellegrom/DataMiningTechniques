5676deadac0dd81622dd7a03226b488f9ec0f1ff
Titus Pellegrom <tituspellegrom@gmail.com>
2021-04-03 12:38:56 +0200

    Feature Correlation
    1) Feature correlation heat map
    2) Start data preprocessing

--- titus_eda.py
+++ titus_eda.py
@@ -4,8 +4,10 @@ import plotly.graph_objects as go
 import plotly.io as pio
 from collections import ChainMap
 pio.renderers.default = "browser"
+import matplotlib.pyplot as plt
 
 DATA_FOLDER = 'data/'
+PLOTS_FOLDER = 'plots/'
 FILE_NAME = 'dataset_mood_smartphone.csv'
 
 df = pd.read_csv(DATA_FOLDER+FILE_NAME, index_col=0, parse_dates=['time'])
@@ -30,21 +32,30 @@ expected_users = {f"AS14.{i+1:02d}" for i in range(n_users)}
 missing_users = expected_users.difference(users)
 
 # Full feature aggregation per day
-group_score = ['mood', 'circumplex.arousal', 'circumplex.valence', 'activity']
-group_count = ['call', 'sms']
-group_time = [col for col in df.columns if col.startswith('appCat.') or col == 'screen']
-
-agg_fn_score = {col: ['min', 'max', 'median', 'mean', 'std', 'count'] for col in group_score}
-agg_fn_count = {col: ['count'] for col in group_score}
-agg_fn_time = {col: ['min', 'max', 'median', 'mean', 'std', 'count', 'sum'] for col in group_time}
-agg_dict = ChainMap(agg_fn_score, agg_fn_count, agg_fn_time)
-
 df_wide = df.copy()
 df_wide = df_wide.pivot_table(index=['id', 'date'], columns='variable', values='value',
                               aggfunc=['min', 'max', 'median', 'mean', 'std', 'count', 'sum'])
 df_wide = df_wide.swaplevel(0, 1, axis=1).sort_index(level='variable', axis=1)
 dups = df_wide[df_wide.index.duplicated(keep=False)]    # suddenly duplicates??
 df_wide.drop_duplicates(keep='last', inplace=True)
+
+# Drop irrelevant aggregations
+group_score = ['mood', 'circumplex.arousal', 'circumplex.valence', 'activity']
+group_count = ['call', 'sms']
+group_time = [col for col in df_wide.columns.get_level_values('variable') if col.startswith('appCat.') or col == 'screen']
+
+agg_fn_score = {col: ['min', 'max', 'median', 'mean', 'std', 'count'] for col in group_score}
+agg_fn_count = {col: ['count', ] for col in group_count}
+agg_fn_time = {col: ['min', 'max', 'median', 'mean', 'std', 'count', 'sum'] for col in group_time}
+agg_dict = ChainMap(agg_fn_score, agg_fn_count, agg_fn_time)
+
+relevant_cols = [(col, fn) for col, fn_lst in agg_dict.items() for fn in fn_lst]
+df_wide = df_wide.loc[:, relevant_cols]
+
+# Maybe day of week contains information (weekend=happy?)
+df_wide['day_of_week'] = df_wide.index.get_level_values(level='date').dayofweek
+df_wide[['mon', 'thue', 'wed', 'thu', 'fri', 'sat', 'sun']] = pd.get_dummies(df_wide['day_of_week']) # saturday happiest
+
 
 # heatmap mood
 df_mood = df_wide.loc[:, ('mood', 'mean')].unstack(level='date').sort_index(level='date', axis=1)
@@ -55,6 +66,42 @@ fig = go.Figure(data=go.Heatmap(z=df_mood.values,
                                 zmid=5.5
                                 ))
 fig.show()
+fig.write_html(PLOTS_FOLDER+"mood_heatmap.html")
+
+
+# Data Relationships
+pd.plotting.scatter_matrix(df_wide.loc[:, [('mood', 'mean'), ('circumplex.arousal', 'mean')]])
+
+
+df_corr = df_wide.groupby('date').mean().corr()
+fig = go.Figure(data=go.Heatmap(z=df_corr.values,
+                                x=list(map(lambda c: '-'.join(c), df_corr.axes[1])),
+                                y=list(map(lambda c: '-'.join(c), df_corr.axes[0])),
+                                ))
+fig.show()
+fig.write_html(PLOTS_FOLDER+"correlation.html")
+
+# Basic ANN
+# TODO: Encode user_id and add to features
+
+df_collapse = df_wide.groupby('date').mean()
+lookback = 3
+l_history = [df_collapse.shift(i+1) for i in range(lookback)]
+
+df_X = pd.concat(l_history, axis=1)
+df_Y = df_collapse[('mood', 'mean')].shift(-1)
+
+# Select rows where target is not NaN
+data_idx = ~df_Y.isna()
+df_X = df_X[data_idx]
+df_Y = df_Y[data_idx]
+
+# Split & Normalizing
+
+
+
+
+
 
 
 
