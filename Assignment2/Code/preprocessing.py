8c52ea05e3aca243c20f77a2442d220fd9b86997
Jaimie <j.r.rutgers@student.vu.nl>
2021-05-01 12:21:45 +0200

    Preprocessing finalized

--- Assignment2/Code/preprocessing.py
+++ Assignment2/Code/preprocessing.py
@@ -3,8 +3,16 @@ import torch.nn as nn
 import torchfm.model.dfm as dfm
 import dask.dataframe as dd
 from tqdm import tqdm
+import numpy as np
+import gc
 
 def get_labels(row):
+    '''
+    Creates label column based on variables 'click_bool' and 'booking_bool. 1 for only click
+    5 for click and book and else 0.
+    :param row: row of the DataFrame
+    :return: either 0, 1 or 5
+    '''
     if row['click_bool'] == 1:
         if row['booking_bool'] == 1:
             return 5
@@ -14,45 +22,76 @@ def get_labels(row):
         return 0
 
 
-print('Loading data...')
-df = pd.read_pickle('df_temporary.pkl')
+def convert_dtypes(df, id_cols):
+    '''
+    Sets dtype of column to minimal required dtype to speed up dummy encoding process
+    :param df: DataFrame containing input data
+    :param id_cols: Column names of columns that need to be converted to one-hot-encoded columns
+    :return: df: DataFrame with adjusted dtypes
+    '''
+
+    for column in id_cols:
+        if df[column].max() <= 255:
+            df[column] = df[column].astype(np.uint8)
+        elif df[column].max() <= 65535:
+            df[column] = df[column].astype(np.uint16)
+        elif df[column].max() <= 4294967295:
+            df[column] = df[column].astype(np.uint32)
+
+    return df
 
 
-#workaround to get categorical columns at end of dataframe
-id_cols = ['srch_id', 'site_id', 'visitor_location_country_id', 'prop_country_id', 'prop_id', 'srch_destination_id']
-non_id_cols = list(set(df.columns)-set(id_cols))
-cols_sorted = []
-cols_sorted.extend(non_id_cols)
-cols_sorted.extend(id_cols)
-df = df.reindex(columns=cols_sorted)
+def one_hot_encode(df, id_cols):
+    '''
+    Creates one-hot encodings of columns containing ids in sparse format. Also stores the feature size of the id column
+    in embedding_dims as this is required input for the DeepFM algorithm. Stores all intermediate one-hot-encoded
+    dataframes in df_dummies_list
+    :param df: input DataFrame
+    :param id_cols: list of column names that should be encoded
+    :return: DataFrame with all encodings
+    '''
 
-print('Creating label...')
-# create label column and drop bool_click and bool_book
-df['label'] = df.apply(lambda row: get_labels(row), axis=1)
-df.drop(['click_bool', 'booking_bool'], axis=1, inplace=True)
+    print('Encoding dummies...')
+    embedding_dims = []
+    df_dummies_list = []
 
-print('Encoding dummies...')
-#create one hot for all id columns
-embedding_dims = []
-df_dummies = pd.DataFrame()
-df_dummies = dd.from_pandas(df_dummies, npartitions=1)
-for column in tqdm(id_cols):
-    column_dummy = pd.get_dummies(df[column], sparse=True, dtype=int)
-    column_dummy = dd.from_pandas(column_dummy, npartitions=1)
-    embedding_dims.append(column_dummy.shape[1])
-    df_dummies = df_dummies.append(column_dummy)
-#
-# df.drop(id_cols, axis=1, inplace=True)
+    for column in tqdm(id_cols):
+        column_dummy = pd.get_dummies(df[column], sparse=True)
+        embedding_dims.append(column_dummy.shape[1])
+        df_dummies_list.append(column_dummy)
 
-# search_dummy = pd.get_dummies(df['srch_id'], sparse=True)
-# search_dummy_first = search_dummy
-# search_emb = nn.Embedding(search_dummy.shape[1], 3)
-#
-# print(search_dummy)
-#
-# df = df.append(search_dummy)
-print(df.head())
+        # force garbage collector to clear memory
+        del column_dummy
+        gc.collect()
 
-# dfm.DeepFactorizationMachineModel()
+    df_dummy = pd.concat(df_dummies_list, axis=1)
 
+    return df_dummy, embedding_dims
 
+def preprocess():
+    '''
+    Preprocesses the input data to a dataframe with all id columns one-hot encoded
+    :return: DataFrame with features and one-hot encoded categories. List of embedding dimensions needed for DeepFM
+    '''
+    print('Loading data...')
+    df = pd.read_pickle('df_temporary.pkl')
+    id_cols = ['srch_id', 'site_id', 'visitor_location_country_id', 'prop_country_id', 'prop_id', 'srch_destination_id']
+    non_id_cols = list(set(df.columns) - set(id_cols))
+
+    # create label column and drop unneeded columns
+    print('Creating label...')
+    df['label'] = df.apply(lambda row: get_labels(row), axis=1)
+    df.drop(['click_bool', 'booking_bool', 'date_time'], axis=1, inplace=True)
+
+    df = convert_dtypes(df, id_cols)
+    df_dummy, embedding_dims_id = one_hot_encode(df, id_cols)
+    data_merged = pd.concat([df, df_dummy], axis=1)
+    data_merged.drop(id_cols, axis=1, inplace=True) #drop id columns as these are now encoded
+
+    data_merged.to_pickle('data_merged.pkl')
+
+    # add feature dims of non_id_cols, this is alway one
+    embedding_dims = [1]*len(non_id_cols)
+    embedding_dims.extend(embedding_dims_id)
+
+    return data_merged, embedding_dims
