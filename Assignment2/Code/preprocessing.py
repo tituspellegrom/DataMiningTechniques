5dc3d7805ee0277312c42d996638077a1ba7f03b
Titus Pellegrom <tituspellegrom@gmail.com>
2021-05-04 10:37:49 +0200

    Preprocessing saved as sparse matrix

--- Assignment2/Code/preprocessing.py
+++ Assignment2/Code/preprocessing.py
@@ -1,11 +1,11 @@
 import pandas as pd
-import torch.nn as nn
-import torchfm.model.dfm as dfm
-import dask.dataframe as dd
 from tqdm import tqdm
 import numpy as np
 import gc
 import pickle
+from sklearn.preprocessing import OneHotEncoder
+import scipy.sparse as sp
+
 
 def create_label_column(df):
     df['label'] = 0
@@ -17,6 +17,19 @@ def create_label_column(df):
 
 
     return df
+
+
+def create_label_column2(df):
+    df['label'] = 0
+    book_indices = df['booking_bool'] == 1
+    click_indices = df['click_bool'] == 1
+
+    df['label'].loc[click_indices] = 1
+    df['label'].loc[book_indices] = 5
+
+
+    return df['label'].to_numpy()
+
 
 def get_labels(row):
     '''
@@ -80,6 +93,74 @@ def one_hot_encode(df, id_cols):
 
     return df_dummy, embedding_dims
 
+
+def one_hot_encode2(X):
+    '''
+    Creates one-hot encodings of numpy array containing categorical data, in sparse format.
+    Also stores the feature size of the categories per column
+    in ids_dims as this is required input for the DeepFM algorithm. Stores all intermediate one-hot-encoded
+    array in a list
+    :param X: input numpy array
+    :return: X_encoded with all encodings, ids_dims as a list containing dimensions
+    '''
+
+    print('Encoding dummies...')
+    X_enc_list = []
+
+    enc = OneHotEncoder()
+
+    n, m = X.shape
+    for i in tqdm(range(m)):
+        X_i = X[:, i].reshape(-1, 1)
+
+        X_i_enc = enc.fit_transform(X_i)
+
+        X_enc_list += [X_i_enc]
+
+    X_enc = sp.hstack(X_enc_list)
+    ids_dims = [X_enc.shape[1] for X_enc in X_enc_list]
+
+    return X_enc, ids_dims
+
+
+def preprocess2(data_name):
+    '''
+    Preprocesses the input data to a dataframe with all id columns one-hot encoded
+    :return: DataFrame with features and one-hot encoded categories. List of embedding dimensions needed for DeepFM
+    '''
+
+    print('Loading data...')
+
+    df = pd.read_pickle(f'{data_name}.pkl')
+    groups = df['srch_id'].to_numpy()
+
+    # id_cols = ['srch_id', 'site_id', 'visitor_location_country_id', 'prop_country_id', 'prop_id', 'srch_destination_id']
+    # pd.DataFrame(id_cols, columns=['id_columns']).to_csv(f'{data_name}_idcols.csv', index=False)
+
+    X_label = create_label_column2(df).reshape(-1, 1)
+    df.drop(['click_bool', 'booking_bool', 'date_time'], axis=1, inplace=True)
+
+    id_cols = pd.read_csv(f'{data_name}_idcols.csv')['id_columns'].values.tolist()
+    non_id_cols = list(set(df.columns) - set(id_cols))
+
+    df = convert_dtypes(df, id_cols)
+
+    X_non_ids = df[non_id_cols].to_numpy()
+    X_ids = df[id_cols].to_numpy()
+
+    X_ids_enc, ids_dims = one_hot_encode2(X_ids)
+    X_enc = sp.hstack([X_non_ids, X_ids_enc, X_label])
+
+    non_ids_dims = [1] * X_non_ids.shape[1]
+    embedding_dims = np.array(non_ids_dims+ids_dims+[1]) # label also dimension 1 => is this needed?
+
+    sp.save_npz(f'{data_name}_data_merged.npz', X_enc)
+    embedding_dims.tofile(f'{data_name}_embedding_dims.txt')
+    groups.tofile(f'{data_name}_groups.txt')
+
+    return X_enc, embedding_dims, groups
+
+
 def preprocess():
     '''
     Preprocesses the input data to a dataframe with all id columns one-hot encoded
@@ -112,8 +193,7 @@ def preprocess():
     data_merged.to_pickle('data_merged.pkl')
 
 
-
-# add feature dims of non_id_cols, this is alway one
+    # add feature dims of non_id_cols, this is alway one
     embedding_dims = [1]*len(non_id_cols)
     embedding_dims.extend(embedding_dims_id)
 
@@ -127,5 +207,7 @@ def preprocess():
 
     return data_merged, embedding_dims, groups
 
+
 if __name__ == '__main__':
-    preprocess()
+    preprocess2('df_temporary')
+
