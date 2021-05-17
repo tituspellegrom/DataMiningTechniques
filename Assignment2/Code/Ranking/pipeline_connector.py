6def6b0f5c1b68a8fc3c99226ccd199e1bec4d19
Dominic Istha <do.istha@gmail.com>
2021-05-17 14:04:15 +0200

    GWO

--- Assignment2/Code/Ranking/pipeline_connector.py
+++ Assignment2/Code/Ranking/pipeline_connector.py
@@ -4,3 +4,35 @@ Created on Sat May 15 13:06:48 2021
 
 @author: doist
 """
+
+import gzip
+import pandas as pd
+import numpy as np
+import copy
+
+def import_file(file):
+    data_file = gzip.open(file)
+    data = np.load(data_file)
+    return data
+
+def prep_data(path, pred_file):
+    # Import data
+    prob_predictions = import_file(path+'val_predictions/'+str(pred_file))
+    ids = pd.read_csv(path+'val_ids.csv', sep=',')
+    actual = np.load(path+'y_val.npy')
+
+    # Prepare data df
+    data = pd.DataFrame(columns=['search_id', 'hotel_id', 'prob0', 'prob1', 'prob5'])
+    data['search_id'] = copy.copy(ids['srch_id'])
+    data['hotel_id'] = copy.copy(ids['prop_id'])
+    data['prob0'] = [item[0] for item in prob_predictions]
+    data['prob1'] = [item[1] for item in prob_predictions]
+    data['prob5'] = [item[2] for item in prob_predictions]
+    
+    # Prepare true_labels df
+    true_labels = pd.DataFrame(columns=['search_id', 'hotel_id', 'true_label'])
+    true_labels['search_id'] = copy.copy(ids['srch_id'])
+    true_labels['hotel_id'] = copy.copy(ids['prop_id'])
+    true_labels['true_label'] = actual
+    
+    return data, true_labels
--- Assignment2/Code/Ranking/ranking_pipeline.py
+++ Assignment2/Code/Ranking/ranking_pipeline.py
@@ -5,11 +5,8 @@ import math
 import pyswarm as ps
 import GWO
 import random
-
-def import_file(file):
-    data_file = gzip.open(file)
-    data = np.load(data_file)
-    return data
+import pipeline_connector
+from tqdm import tqdm
 
 def determine_true_rankscore(x):
     if x['booking_bool'] == 1:
@@ -20,6 +17,9 @@ def determine_true_rankscore(x):
         return 0
 
 def prep_true_labels(true_labels):
+    '''
+    Function to transform 'click_bool' and 'booking_bool' into 1 column with 0, 1 or 5
+    '''
     true_labels['true_label'] = true_labels.apply(lambda x: determine_true_rankscore(x), axis=1)
     true_labels = true_labels.drop(columns=['click_bool', 'booking_bool'], axis=1)
     return true_labels
@@ -37,38 +37,42 @@ def dcg_ideal(temp_true):
     '''
     Calculate CDG of ideal ranking
     '''
+    
     ideal = temp_true.sort_values(by=['true_label'], ascending=False)
     ideal = ideal['true_label'][:5].values
-    return ideal[0] + (ideal[1]/math.log(2,2)) + (ideal[2]/math.log(3,2)) + (ideal[3]/math.log(4,2))
+    return 0.0001 + ideal[0] + (ideal[1]/math.log(2,2)) + (ideal[2]/math.log(3,2)) + (ideal[3]/math.log(4,2))
 
 
 def evaluate_result(result, true_labels):
     '''
     Evaluate predicted rank based on true labels, using NDCG.
     '''
+    
+    print('\nEvaluating result...\n')
     results = []
     
-    for search_id in result['search_id'].unique():
+    for search_id in tqdm(result['search_id'].unique()):
         temp_true = true_labels[true_labels['search_id']==search_id]
         idcg = dcg_ideal(temp_true)
         
         temp_pred = result[result['search_id']==search_id]
         dcg = dcg_prediction(temp_pred, temp_true)
-        
         ndcg = dcg/idcg
         results.append(ndcg)
 
-    return sum(results)/len(results)
+    return -sum(results)/len(results)
     
 
 def rank_output(data):
     '''
     Rank hotel_id's per search_id based on the rankscore.
     '''
+    
+    print('\nRanking the data...\n')
     res_search_ids = []
     res_hotel_ids = []
     
-    for search_id in data['search_id'].unique():
+    for search_id in tqdm(data['search_id'].unique()):
         temp = data[data['search_id']==search_id]
         temp = temp.sort_values(by=['rankscore'], ascending=False)
         res_search_ids.append(temp['search_id'].values)
@@ -110,8 +114,8 @@ def grey_wolf_optimization(fitness_function, data, true_labels):
     
     print("Goal is to optimize weights w1, w2, w3 that determine the rankscore")
     
-    num_particles = 50
-    max_iter = 30
+    num_particles = 10
+    max_iter = 15
     
     print("Setting num_particles = " + str(num_particles))
     print("Setting max_iter    = " + str(max_iter))
@@ -119,35 +123,38 @@ def grey_wolf_optimization(fitness_function, data, true_labels):
     print("Upper bound weights = " + str(maxW))
     print("\nStarting GWO algorithm\n")
     
-    best_position = GWO.gwo(fitness_function, max_iter, num_particles, dim, minW, maxW, data, true_labels)
+    best_sol = GWO.gwo(fitness_function, max_iter, num_particles, dim, minW, maxW, data, true_labels)
     
     print("\nGWO completed\n")
     print("\nBest solution found:")
-    print(["%.6f"%best_position[k] for k in range(dim)])
-    err = fitness(best_position, data, true_labels)
+    print(["%.6f"%best_sol.weights[k] for k in range(dim)])
+    err = fitness(best_sol.weights, data, true_labels)
     print("fitness of best solution = %.6f" % err)
       
-    print("\nEnd GWO for rastrigin\n")
-    return
+    print("\nEnd GWO\n")
+    return best_sol
 
 
 def main():
-    path = "C:/Users/doist/OneDrive/Documenten/Business Analytics/Master/Year 1/Data Mining Techniques/Assignment 2/Data/"
-    data = pd.read_csv(path+'simple_example.csv', sep=',')
-    
-    true_labels = pd.read_csv(path+'simple_example_true_labels.csv')
-    true_labels = prep_true_labels(true_labels)
-    
+    path = 'C:/Users/doist/OneDrive/Documenten/GitHub/DataMiningTechniques/Assignment2/Final Code/'
+    data, true_labels = pipeline_connector.prep_data(path, '/GradientBoostingClassifier_proba.npy.gz')
+
+    x = data[:10000]
+    y = true_labels[:10000]
+
     weights = init_weights()
+    #weights = [-8.189630, -8.222910, -31.149832]
+    '''
     data['rankscore'] = rank_score(data, weights)
     
     result = rank_output(data)
     
     ndcg = evaluate_result(result, true_labels)
-    print(ndcg)
+    print('\nObtained NCDG = ', ndcg)
+    '''
     
-    grey_wolf_optimization(fitness, data, true_labels)
-
+    best = grey_wolf_optimization(fitness, data, true_labels)
+    return
     
 
 if __name__ == '__main__':
